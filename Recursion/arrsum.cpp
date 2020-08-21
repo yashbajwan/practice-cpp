3286fb0341b635a4d0a4da8aeab19f707787e6c4
yash yash <yash8135512@gmail.com>
2020-08-22 01:45:43 +0530

    inital comit

---
+++ Recursion/arrsum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr(int a[], int n)
+{
+    if (n <= 0)
+        return 0;
+    return a[n - 1] + arr(a, n - 1);
+}
+
+int main()
+{
+    int a[10], n;
+    cin >> n;
+    cout << endl;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    cout << "\n";
+    cout << arr(a, n);
+}
