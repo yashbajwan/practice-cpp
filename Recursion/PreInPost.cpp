bb852640efb91131b06d274057e960e07eb1fe3d
yash yash <yash8135512@gmail.com>
2020-09-01 11:16:15 +0530

    working fine

--- Recursion/PreInPost.cpp
+++ Recursion/PreInPost.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+void fun(int a)
+{
+    if (a <= 0)
+        return;
+    cout << a << " Pre Hello" << endl;
+
+    fun(a - 1);
+    cout << a << " In Hi" << endl;
+
+    fun(a - 2);
+    cout << a << " Post bye" << endl;
+}
 
 int main()
 {
-
-    return 0;
+    fun(5);
 }
