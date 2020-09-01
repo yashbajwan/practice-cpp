26163e19dca2c20f9d427615627c23820b2bf360
yash yash <yash8135512@gmail.com>
2020-09-01 11:09:49 +0530

    PreInPost added

--- Recursion/euler3.cpp
+++ Recursion/euler3.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void euler3(int n, int level)
+{
+    if (n <= 2)
+    {
+        cout << "Base: " << n * level << endl;
+        return;
+    }
+
+    cout << "Pre: " << n * level << endl;
+    euler3(n - 1, level + 1);
+
+    cout << "In1: " << n * level << endl;
+    euler3(n - 2, level + 1);
+
+    cout << "In2: " << n * level << endl;
+    euler3(n - 3, level + 1);
+
+    cout << "Post: " << n * level << endl;
+}
+
 int main()
 {
-
-    return 0;
+    euler3(5, 5);
 }
